0f7250d9af1d771429a9096691fe0203e2756eaf
sj141200 <118284421+sj141200@users.noreply.github.com>
2023-07-13 15:00:32 +0530

    Signup.js

---
+++ Signup.js
@@ -0,0 +1,72 @@
+
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import styles from "./Signup.module.css"
+import Inputform from './Inputform'
+import { Link } from 'react-router-dom'
+import { auth } from './firebase'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import './Signup.module.css'
+export default function Signup() {
+  const [values,setValues] = useState({
+    name:"",
+    email:"",
+    password:""
+  })
+
+  const[btnDisabled,setBtnDisabled] = useState(false)
+
+  const navigate = useNavigate()
+  const [errMsg,setErrMsg] = useState("")
+  const handleSubmission =()=>{
+    if(!values.name || !values.email ||!values.password){
+      setErrMsg("Fill All Fields")
+      setBtnDisabled(true)
+      return
+    }
+    setErrMsg("")
+    createUserWithEmailAndPassword(auth, values.email, values.password).then((res)=>{
+      const user = res.user;
+      console.log(user)
+      updateProfile(user,{
+        displayName : values.name
+      })
+      navigate('./login')
+    }).catch((err)=>{
+      setBtnDisabled(false)
+      setErrMsg(err.message)
+    })
+  }
+  return (
+  
+      <div className={styles.container}>
+       
+        <div className={styles.innerBox}>
+          <h1 className={styles.heading}>Sign Up</h1>
+          <Inputform type="text" label="Name" placeholder="Enter Your Name"
+          onChange={(event)=>
+          setValues((prev)=>({...prev,name:event.target.value}))
+          }/>
+          <Inputform type="email" label="Email" placeholder="Enter Your Email"
+          onChange={(event)=>
+          setValues((prev)=>({...prev,email:event.target.value}))}/>
+          <Inputform type="password" label="Password" placeholder="Enter Your Password"
+          onChange={(event)=>
+          setValues((prev)=>({...prev,password:event.target.value}))}
+          />
+          <div className={styles.footer}>
+            <b className={styles.error}>{errMsg}</b>
+            <button onClick={handleSubmission} disabled={btnDisabled}>SignUp</button>
+           <div className={styles.account}>
+              <p>Already have an Account?</p>
+              <span>
+                  <Link className={styles.link} to="/login">Login</Link>
+               </span>
+           </div>
+          </div>
+        </div>
+
+      </div>
+   
+  )
+}
